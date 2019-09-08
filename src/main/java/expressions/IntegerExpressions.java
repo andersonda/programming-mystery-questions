e950f77b0a6d4cad85ce2dc7c052b6e37f193b71
David Anderson <andersonda8109@gmail.com>
2019-09-07 21:26:47 -0700

    initial project files

---
+++ src/main/java/expressions/IntegerExpressions.java
@@ -0,0 +1,11 @@
+package expressions;
+
+public class IntegerExpressions {
+    public static void main(String[] args){
+        System.out.println(2 + 3 * 4 - 6);
+        System.out.println(14 / 7 * 2 + 30 / 5 + 1);
+        System.out.println(6/2 + 7/3);
+        System.out.println((238 % 10 + 3) % 7);
+        System.out.println((18 - 7) * (43 % 10));
+    }
+}
