43926339810864a470d21fcb0349c49ed25b0897
David Anderson <andersonda8109@gmail.com>
2019-09-23 13:16:33 -0700

    additional simple example

--- src/main/java/expressions/Hello.java
+++ src/main/java/expressions/Hello.java
@@ -2,6 +2,6 @@ package expressions;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, \nWorld!");
+        System.out.println("Hello, World!");
     }
 }
